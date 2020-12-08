e57bb7275a5b8a5a30b40f271a002023a621b66f
torigoshi koki <k.torigoshi@ichain.co.jp>
2020-12-08 15:58:00 +0900

    bit_rise

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("bit rise"),
+        title: Text("テスト"),
       ),
       body: Center(
         child: Column(
