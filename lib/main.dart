4f2ca11439624e76c54d6bb3a72101c187433741
torigoshi koki <k.torigoshi@ichain.co.jp>
2020-12-09 12:53:31 +0900

    test

--- lib/main.dart
+++ lib/main.dart
@@ -64,7 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("テスト"),
+        title: Text("テスト中"),
       ),
       body: Center(
         child: Column(
